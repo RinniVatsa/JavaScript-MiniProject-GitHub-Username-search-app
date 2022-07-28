7e5a23351177a47f3263754b870c9ed0a3a2af7e
Rinni Vatsa <101491369+RinniVatsa@users.noreply.github.com>
2022-07-29 02:32:05 +0530

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,37 @@
+### GitHub User Search App
+
+## By Rinni Vatsa
+
+#### This Webpage is showing information about GitHub User Respository and GitHub Profile
+
+## The project description in the text box below:
+Web Based GitHub Search App. It is an app based on HTML CSS and JS which find the GitHub User from the internet.
+
+GitHub Search App is a simple and efficient web based application and also responsive for all smartphones.
+
+## Setup/Installation Requirements
+
+* Clone this repo to your desktop
+* Naviagte to the top level of directory
+* Open index.html in your browser
+
+## WHY this project was chosen (what use case does it solve)
+
+This project was chosen for searching a GitHub user from web.
+
+## WHAT features, technical things you had implemented
+
+* HTML
+* CSS
+* Javascript
+* Api (Little)
+
+## HOW did you make the project (Tech Stack, Database, APIs)
+ 
+* HTML
+* CSS
+* Javascript
+* Api (Little)
+
+# Note 
+This site best viewed in Google Chrome browser
---
+++ app.js
@@ -0,0 +1,50 @@
+const input = document.querySelector('input')
+const btn = document.querySelector('button')
+const card = document.querySelector('.card')
+
+user('RinniVatsa')
+async function user (username) {
+    const resp = fetch(`http://api.github.com/users/${username}`)
+    const respData = (await resp).json()
+    console.log(respData);
+    return respData
+}
+
+btn.addEventListener('click', async() => {
+    const input_val = input.value
+    const search_result = await user(input_val)
+    console.log(search_result);
+    if(!search_result.login){
+        alert('No User Found')
+    }else{
+        card.innerHTML = `
+        <div class="avatar">
+            <img src="${search_result.avatar_url}" alt="">
+        </div>
+            
+        <div class="info">
+            <h2>${search_result.name}</h2>
+            <p>${search_result.login}</p>
+
+        <div class="follow-info">
+        <div class="single">
+            <span>${search_result.followers}</span>
+            <span>Followers</span>
+        </div>
+        <div class="single">
+            <span>${search_result.following}</span>
+            <span>Following</span>
+        </div>
+        </div>
+
+        <div class="single">
+            <span>${search_result.public_repos}</span>
+            <span>Repositories</span>
+        </div>
+        </div>
+            <a href="${search_result.html_url}" target="_blank">Visit Github Profile ></a>
+        </div>
+        </div>
+        `
+    }
+})
